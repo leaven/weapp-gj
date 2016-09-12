802722afa0b7ae8db0d71587d3e3ca89711cc0a6
canhuang <canhuang@git.code.com>
2016-09-12 20:50:59 +0800

    修改

--- utils/util.js
+++ utils/util.js
@@ -10,6 +10,7 @@ function formatTime(date) {
 
   return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
 }
+
 function formatNumber(n) {
   n = n.toString()
   return n[1] ? n : '0' + n
